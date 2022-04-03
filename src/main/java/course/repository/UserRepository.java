56fb8ff671297532f9ee6e866daee1fc2b26c64c
Bernardo Maggessi <bermaggessi2@yahoo.com>
2022-04-03 15:31:41 -0300

    obtendo usuario por id

--- src/main/java/course/repository/UserRepository.java
+++ src/main/java/course/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package course.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import course.domain.User;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String>{
 
 }
---
+++ src/main/java/course/resources/exception/StandardError.java
@@ -0,0 +1,68 @@
+package course.resources.exception;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long timeStamp;
+	private Integer status;
+	private String error;
+	private String messsage;
+	private String path;
+	
+	public StandardError() {
+	}
+
+	public StandardError(Long timeStamp, Integer status, String error, String messsage, String path) {
+		super();
+		this.timeStamp = timeStamp;
+		this.status = status;
+		this.error = error;
+		this.messsage = messsage;
+		this.path = path;
+	}
+
+	public Long getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(Long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMesssage() {
+		return messsage;
+	}
+
+	public void setMesssage(String messsage) {
+		this.messsage = messsage;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+
+}
