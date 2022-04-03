98e6dd8ee72c8026f7190a676eaf7956a833cc16
Bernardo Maggessi <bermaggessi2@yahoo.com>
2022-04-03 14:50:27 -0300

    operacao de instanciacao de banco de dados

---
+++ src/main/java/course/config/Instantiation.java
@@ -0,0 +1,31 @@
+package course.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import course.domain.User;
+import course.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		//instancia os objetos para testar banco de dados
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
