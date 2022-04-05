1c6b1c27f6f73e929f2457416df8f8991ffd4ae7
Bernardo Maggessi <bermaggessi2@yahoo.com>
2022-04-05 11:19:28 -0300

    consultas com vários critérios

--- src/main/java/course/repository/PostRepository.java
+++ src/main/java/course/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package course.repository;
 
+import java.util.Date;
 import java.util.List;
+
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
@@ -10,8 +12,12 @@ import course.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-	
+
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
